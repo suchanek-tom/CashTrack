466d4b332f9fde2ff9a836322d7787352f8c930e
Tomáš Suchanek <tomassuchanek96@gmail.com>
2025-03-02 22:05:08 +0100

    Initial commit for CashTrack

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button, HStack } from "@chakra-ui/react"
+
+export const App = () =>{
+  return(
+    <> 
+      <h1>Test</h1>
+      <HStack>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
+    </>
+  )
+}
